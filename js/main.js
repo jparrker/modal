cfad3927bd08f0c6653138be42c034048509c62b
jparrker <102701917+jparrker@users.noreply.github.com>
2023-11-08 18:32:48 -0500

    added ability to close modal while clicking outside modal

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const modalButton = document.querySelector(".jsModalButton");
 const modalCloseButton = document.querySelector(".jsModalClose");
+const modalOverlay = document.querySelector(".modal-overlay");
 
-modalButton.addEventListener("click", (e) => {
+modalButton.addEventListener("click", (event) => {
   document.body.classList.add("modal-is-open");
 });
 
-modalCloseButton.addEventListener("click", (e) => {
+modalCloseButton.addEventListener("click", (event) => {
   document.body.classList.remove("modal-is-open");
+});
+
+modalOverlay.addEventListener("click", (event) => {
+  if (!event.target.closest(".modal")) {
+    document.body.classList.remove("modal-is-open");
+  }
 });
